28b69fb1f7b527dba8b2ba90e5cf216fb29c2e51
cauxNicolas <nicaux95@gmail.com>
2021-02-04 22:59:17 +0100

    regExp - contact - mongo OK

--- routes/Contact.js
+++ routes/Contact.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const Client = require("../model/Client");
 
 router.post("/contact", async (req, res) => {
+	// regExp
 	const validateEmail = (value) => {
 		const re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+		return re.test(String(value).toLowerCase());
+	};
+
+	const validateInput = (value) => {
+		const re = /^[a-z0-9_-]{3,30}$/;
 		return re.test(String(value).toLowerCase());
 	};
 
@@ -14,31 +20,43 @@ router.post("/contact", async (req, res) => {
 		req.fields.email !== "" &&
 		req.fields.textarea !== ""
 	) {
-		try {
-			const name = req.fields.name;
-			const lastname = req.fields.lastname;
-			const email = req.fields.email;
-			const textarea = req.fields.textarea;
+		const name = req.fields.name;
+		const lastname = req.fields.lastname;
+		const email = req.fields.email;
+		const textarea = req.fields.textarea;
 
-			const testEmail = validateEmail(email);
-			if (testEmail === true) {
-				const newClient = new Client({
-					name,
-					lastname,
-					email,
-					textarea,
-				});
-				// await newClient.save();
-				res.status(200).json(
-					"Merci ! Votre message est bien enregistré"
-				);
+		const testEmail = validateEmail(email);
+		const testName = validateInput(name);
+		const testLastname = validateInput(lastname);
+
+		if (testEmail === true && testName === true && testLastname === true) {
+			const search = await Client.findOne({ email: email });
+			// si client envoie un 2e message
+			if (search) {
+				search.textarea.push(textarea);
+				await search.save();
+				res.status(200).json("Votre message à bien été rajouté !");
 			} else {
-				res.status(400).json({
-					message: "Merci de remplir les champs correctement",
-				});
+				try {
+					console.log(testEmail, testName);
+					const newClient = new Client({
+						name,
+						lastname,
+						email,
+						textarea,
+					});
+					await newClient.save();
+					res.status(200).json(
+						"Merci ! Votre message est bien enregistré"
+					);
+				} catch (error) {
+					res.status(400).json({ message: error.message });
+				}
 			}
-		} catch (error) {
-			res.status(400).json({ message: error.message });
+		} else {
+			res.status(400).json({
+				message: "Merci de remplir les champs correctement",
+			});
 		}
 	} else {
 		res.status(400).json("merci de remplir les champs");
