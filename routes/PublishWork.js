a6b79b1454518575d56eac3c60428512d05e631b
cauxNicolas <nicaux95@gmail.com>
2021-02-17 22:58:22 +0100

    cloudinary cover OK

--- routes/PublishWork.js
+++ routes/PublishWork.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router();
+
 // Function
 const isAuthenticated = require("../function/isAuthenticated");
 
-router.post("/publish-work", isAuthenticated, (req, res) => {
-	/* console.log("req.fields", req.fields);
-		console.log("req.files", req.files); */
+// Cloudinary
+const cloudinary = require("cloudinary").v2;
+
+// Cloudinary config
+cloudinary.config({
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.API_KEY,
+	api_secret: process.env.API_SECRET,
+});
+
+router.post("/publish-work", isAuthenticated, async (req, res) => {
 	try {
-		//creator: req.publish
+		// cover
+		const coverToUpload = req.files.valueCover.path;
+		const result = await cloudinary.uploader.upload(coverToUpload);
+		console.log(result);
+		// slider
+
+		// titre description
+		const objCheck = req.fields;
+
+		// checkbox
+		const tabObjCheck = Object.values(objCheck);
+		// console.log(tabObjCheck);
+
 		res.status(200).json("routes addworks ok");
 	} catch (error) {
 		res.status(400).json({ message: error.message });
