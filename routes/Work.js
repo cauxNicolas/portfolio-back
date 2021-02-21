f77edebaf6de97ab50ad854ea34563990c8d3b00
cauxNicolas <nicaux95@gmail.com>
2021-02-22 00:23:22 +0100

    work params

--- routes/Work.js
+++ routes/Work.js
@@ -4,7 +4,10 @@ const Work = require("../model/Work");
 
 router.get("/work/:id", async (req, res) => {
 	try {
-		res.status(200).json("ok pour la route /:id");
+		if (req.params.id) {
+			const searchId = await Work.findById({ _id: req.params.id });
+			res.status(200).json(searchId);
+		}
 	} catch (error) {
 		res.status(400).json({ message: error.message });
 	}
