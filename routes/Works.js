89610c6e3c6b3f35703ef604620a843ce65651b3
cauxNicolas <nicaux95@gmail.com>
2021-02-21 23:47:56 +0100

    sort test

--- routes/Works.js
+++ routes/Works.js
@@ -4,7 +4,7 @@ const Work = require("../model/Work");
 
 router.get("/", async (req, res) => {
 	try {
-		const data = await Work.find().sort();
+		const data = await Work.find().sort("-created");
 		res.status(200).json(data);
 	} catch (error) {
 		res.status(400).json({ message: error.message });
