6ec968ed682d7966888322f7aafa85522fe13dbc
cauxNicolas <nicaux95@gmail.com>
2021-02-21 01:05:17 +0100

    work.find()

--- routes/Works.js
+++ routes/Works.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const work = require("../model/Work");
 
-router.get("/");
+router.get("/", async (req, res) => {
+	try {
+		const data = await work.find();
+		res.status(200).json("ok tu recup la page works");
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
+});
 
 module.exports = router;
