34b1857c9c603c0c82f2544c69f86e9ea8fb376d
cauxNicolas <nicaux95@gmail.com>
2021-02-22 11:17:57 +0100

    works:/id fonctionnel

--- routes/Work.js
+++ routes/Work.js
@@ -3,13 +3,11 @@ const router = express.Router();
 const Work = require("../model/Work");
 
 router.get("/work/:id", async (req, res) => {
-	console.log(req.params);
 	try {
-		/* if (req.params.id) {
+		if (req.params.id) {
 			const searchId = await Work.findById({ _id: req.params.id });
 			res.status(200).json(searchId);
-		} */
-		res.status(200).json("ok work/:id");
+		}
 	} catch (error) {
 		res.status(400).json({ message: error.message });
 	}
